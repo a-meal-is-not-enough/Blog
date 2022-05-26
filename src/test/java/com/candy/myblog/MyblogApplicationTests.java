2daedd028eda81fc7d184644fdb2821052d71605
a-meal-is-not-enough <sixcandy@126.com>
2022-05-26 19:46:40 +0800

    1.0v

---
+++ src/test/java/com/candy/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candy.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
